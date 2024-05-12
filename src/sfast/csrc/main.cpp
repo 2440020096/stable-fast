3e2d3fac62588b3f74e7f874944dd3834f308561
2440020096 <kay.leonico@binus.ac.id>
2024-05-12 15:46:29 +0700

    unlink cudnn, cublas, and cutlass

--- src/sfast/csrc/main.cpp
+++ src/sfast/csrc/main.cpp
@@ -2,10 +2,10 @@
 
 #include "jit/init.h"
 #include "misc.h"
-#include "operators/cudnn/cudnn_convolution.h"
-#include "operators/cublas/cublas_gemm.h"
-#include "operators/cutlass/cutlass_qlinear.h"
-#include "operators/cutlass/cutlass_dual_linear.h"
+// #include "operators/cudnn/cudnn_convolution.h"
+// #include "operators/cublas/cublas_gemm.h"
+// #include "operators/cutlass/cutlass_qlinear.h"
+// #include "operators/cutlass/cutlass_dual_linear.h"
 #include "operators/fused_linear.h"
 
 using namespace sfast;
@@ -15,10 +15,10 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   misc::initMiscBindings(m);
 }
 
-TORCH_LIBRARY(sfast, m) {
-  operators::initCUDNNConvolutionBindings(m);
-  operators::initCUBLASGEMMBindings(m);
-  operators::initCutlassQLinearBindings(m);
-  operators::initCutlassDualLinearBindings(m);
-  operators::initFusedLinearBindings(m);
-}
+// TORCH_LIBRARY(sfast, m) {
+//   operators::initCUDNNConvolutionBindings(m);
+//   operators::initCUBLASGEMMBindings(m);
+//   operators::initCutlassQLinearBindings(m);
+//   operators::initCutlassDualLinearBindings(m);
+//   operators::initFusedLinearBindings(m);
+// }
